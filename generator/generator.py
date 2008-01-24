5be87c9900f76c31f0912db02bfc9ff3b42642d2
Petr Sykora <petr.sykora@gmail.com>
2008-01-24 22:15:10 +0100

    zelena louka

--- generator/generator.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from math import *
-def f(x,o,u,c):
-	return 1./o/sqrt(2*pi)*exp(0.0-(x-u)*(x-u)/4./o/o)*c
-
-for xx in range(1,30000):
-	x=xx/100.
-	print  f(x,5,40,10)+sin(x*2/20.*pi)*0.1-f(x,5,160,10)
--- tb2musps.pro
+++
@@ -1,3 +0,0 @@
-function tb2musps, tb
-	if ( tb EQ 5 ) then return, 20.
-end
